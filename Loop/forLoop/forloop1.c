11ada62e9f4181469c445ec2c218c00ca3a78025
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-07 16:57:47 +0530

    for loop

---
+++ Loop/forLoop/forloop1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+
+    int i;
+
+    // for(i=1;i<=100;i++){
+    //     printf(" %d",i);
+    // }
+
+
+    // for(i=1;i<=15;i++){
+
+    //     printf("\nHello Good mornign all...");
+    // }
+
+
+    // for(i=19;i<=30;i++)
+    // {
+    //     printf("%d",i);
+    // }
+
+}
