697402f6dd0e7f9f2178915f45182214e83d96cb
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-09 14:20:51 +0530

    triangle

---
+++ Loop/forLoop/forloop4.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+void main(){
+
+
+    int n;
+
+    printf("Enter row number :");
+    scanf("%d",&n);
+    
+
+    
+    for(int i=1;i<=n;i++){
+
+        for(int j=1;j<=i;j++){
+
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int n;
+    // printf("Enter the number  :");
+    // scanf("%d",&n);
+
+
+    // square
+    // for(int i=1;i<=n;i++){
+
+    //     for(int j=1;j<=n;j++){
+
+    //         printf("%c",j+96);
+    //     }
+    //     printf("\n");
+    // }
+}
