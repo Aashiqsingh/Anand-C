4f2b437d453ccef17fb6bf4bd16ca55d5741f51e
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-09 14:53:20 +0530

    holoSquare

--- Loop/forLoop/forloop4.c
+++ Loop/forLoop/forloop4.c
@@ -6,17 +6,37 @@ void main(){
 
     printf("Enter row number :");
     scanf("%d",&n);
-    
 
-    
-    for(int i=1;i<=n;i++){
 
-        for(int j=1;j<=i;j++){
 
+    // ulta triangle
+     for(int i=1;i<=n;i++){
+
+        for(int j=n;j>=i;j--){
             printf("*");
         }
         printf("\n");
     }
+
+
+
+
+
+
+
+
+
+    
+
+    
+    // for(int i=1;i<=n;i++){
+
+    //     for(int j=1;j<=i;j++){
+
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
 
 
 
