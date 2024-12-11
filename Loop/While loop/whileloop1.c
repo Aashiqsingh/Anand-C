3e61dff8299e5074492d6624aa5080841291bef5
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-11 14:19:00 +0530

    while loop

---
+++ Loop/While loop/whileloop1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+
+    //    int i=0; 
+
+    //    while(i<5)
+    //    {
+    //         printf("*");
+    //         i++;
+    //    }
+
+    int start,end;
+
+    printf("Enter a starting number: ");
+    scanf("%d",&start);
+    printf("Enter an ending number: ");
+    scanf("%d",&end);
+
+    int i=start;
+
+    while(i<=end){
+
+        printf("%d ",i);
+        i++;
+    }
+
+
+
+
+
+    // int i=1;
+    // while(i<=num){
+
+    //     printf("*\n");
+    //     i++;
+    // }
+
+
+}
